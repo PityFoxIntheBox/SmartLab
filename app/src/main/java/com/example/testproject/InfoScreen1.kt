20014c881b2b4e0458cee6e8f81a670ed0e37d8b
unknown <@ngknn.local>
2023-10-07 13:17:52 +0300

    .

---
+++ app/src/main/java/com/example/testproject/InfoScreen1.kt
@@ -0,0 +1,11 @@
+package com.example.testproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoScreen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_screen1)
+    }
+}
