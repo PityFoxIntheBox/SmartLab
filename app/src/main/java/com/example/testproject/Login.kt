90f544968a28cacd5f38c8fcc68974b3120c70b9
ЕрмолинИД <blabla@gmail.com>
2023-10-10 14:59:42 +0300

    Добавлены новые страницы

--- app/src/main/java/com/example/testproject/Login.kt
+++ app/src/main/java/com/example/testproject/Login.kt
@@ -2,10 +2,27 @@ package com.example.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.testproject.databinding.ActivityLoginBinding
 
-class Login : AppCompatActivity() {
+class Login : AppCompatActivity(),TextWatcher {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        var binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.editTextTextEmailAddress.addTextChangedListener(this)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+
     }
 }
---
+++ app/src/main/java/com/example/testproject/code_input.kt
@@ -0,0 +1,13 @@
+package com.example.testproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Timer
+
+class code_input : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_input)
+    }
+
+}
