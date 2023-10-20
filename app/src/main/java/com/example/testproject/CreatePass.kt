5e579fec8ef3420419e589b8bfde3ca0b22cd268
ЕрмолинИД <blabla@gmail.com>
2023-10-20 11:54:16 +0300

    Добавлена страница создания пароля

---
+++ app/src/main/java/com/example/testproject/CreatePass.kt
@@ -0,0 +1,11 @@
+package com.example.testproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_pass)
+    }
+}
