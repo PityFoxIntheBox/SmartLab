00baa7ac67f651c59b4e4d3b8d53344c2b478aa3
ЕрмолинИД <blabla@gmail.com>
2023-10-20 11:51:13 +0300

    Добавлены новые страницы

--- app/src/main/java/com/example/testproject/code_input.kt
+++ app/src/main/java/com/example/testproject/code_input.kt
@@ -2,12 +2,33 @@ package com.example.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.Editable
+import com.example.testproject.databinding.ActivityCodeInputBinding
+import com.example.testproject.databinding.ActivityLoginBinding
 import java.util.Timer
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class code_input : AppCompatActivity() {
+
+    var binding : ActivityCodeInputBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_code_input)
+        binding = ActivityCodeInputBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        timer.start()
     }
+    val timer = object : CountDownTimer(60000,1000)
+    {
+        override fun onTick(millisUntilFinished: Long) {
+            binding!!.count.text = "Отправить код повторно можно будет через " + millisUntilFinished.milliseconds.inWholeSeconds + " секунд"
+        }
 
+        override fun onFinish() {
+            start()
+        }
+    }
 }
